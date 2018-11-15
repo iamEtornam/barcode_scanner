8f1ec824e25b34609de6ec6e798516ebd43701d6
regnex <sunumacbright@gmail.com>
2018-11-15 12:14:26 +0000

    added scan functionality for floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(new MyApp());
 
@@ -23,22 +25,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+String result = "Welcome to Barcode Scanner";
+
+Future _scanQR() async{
+  try{
+    String qrResult = await BarcodeScanner.scan();
+    setState(() {
+          result = qrResult;
+        });
+  }on PlatformException catch(ex){
+    if(ex.code == BarcodeScanner.CameraAccessDenied)
+{
+setState(() {
+   result = "Camera permission was Denied"; 
+});
+}
+else{
+  setState(() {
+      result = "Unknown Error $ex";
+    });
+} 
+ }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Barcode Scanner"),
       leading: Icon(Icons.camera),),
       body: Center(
-        child: Text("Welcome to Barcode Scanner",
+        child: Text(
+          result,
         style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
         textAlign: TextAlign.center,),
       ),
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(Icons.camera_alt),
         label: Text("Scan"),
-        onPressed: (){
-
-        },
+        onPressed: _scanQR,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       );
